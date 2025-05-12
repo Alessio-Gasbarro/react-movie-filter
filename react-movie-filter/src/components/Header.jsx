7f74da4cd687b6de2e5e03c484333838e5a97fb9
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-12 20:00:12 +0200

    [UPD] Finalized header

---
+++ react-movie-filter/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <h1>La mia libreria</h1>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
